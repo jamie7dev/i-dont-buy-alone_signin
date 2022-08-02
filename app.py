400c09e972f1377529c704277bd5c53874657ed8
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-08-02 15:08:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ SECRET_KEY = 'SPARTA'
 
 
 client = MongoClient('localhost', 27017)
+# client = MongoClient('mongodb://test:test@localhost', 27017, username="sparta", password="sparta")
 db = client.dbsparta_plus_week4
 
 # client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
